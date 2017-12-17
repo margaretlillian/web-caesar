1171928917cf9dbc3f868e61463b0990331094c4
Margaret Kiley <mlilliankiley@gmail.com>
2017-12-17 05:42:22 -0500

    Simplified things

--- main.py
+++ main.py
@@ -4,66 +4,63 @@ from caesar import rotate_string
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-header = """<!DOCTYPE html>
+form = """<!DOCTYPE html>
 <html>
     <head>
     <title>Caesar Cipher</title>
     <style type="text/css">
-    body {
+    body {{
         background: #f9f9f9;
         color: #666;
         font: lighter 1.5em arial, sans-serif;
-    }
-    a{
+    }}
+    a{{
         color: #117a9e;
         font-weight: bold;
         text-decoration: none;
         border-bottom: 1px solid #536870;
-    }
-    p, input {
+    }}
+    p, input {{
         font-size: 0.75em;
-    }
-    h1 {
+    }}
+    h1 {{
         text-align: center;
         text-transform: uppercase;
         font-weight: lighter;
         padding-bottom: 0;
         margin-bottom: 0;
-    }       
-    h2 {
+    }}    
+    h2 {{
         font-weight: lighter;
-    }
-    div {
+    }}
+    div {{
         margin: 10px auto;
         width: 540px;
         border-radius: 10px;
         background-color: #ced9db;
         padding: 20px;
-                }
-            input, textarea {
+                }}
+            input, textarea {{
                 background: #edf9fc;
                 border: 1px solid #0e82a0;
                 color: #33535b;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 500px;
                 height: 120px;
                 font: 1em arial, sans-serif;
-            }
+            }}
+            .error {{
+                color: #f00;
+            }}
     </style>
     </head>
 
     <body>
         
         <h1>Caesar Cipher</h1>
-        <div>"""
-footer = """
-        </div>
-    </body>
-</html>"""
-
-form = """{HEADER}
+        <div>
         <form method="post">
             <p>The Caesar Cipher rotates each character in the message x number of times.
             For example, a rotation of 1 would make the word "thing" become "uijoh". 
@@ -71,30 +68,22 @@ form = """{HEADER}
             <label>
             Rotate by: <input type="text" name="rot">
             </label>
-
+            {ERROR}
             <label>
-            <br><br>Enter the message to encrypt.<br>
-            <textarea name="text">{TEXT}
- </textarea>
+            <p>Enter the message to encrypt.</p>
+            <textarea name="text">{TEXT}</textarea>
             </label>
             
             <input type="submit" value="Encrypt">
         </form>
-{FOOTER}"""
-
-
-error = """{HEADER}
-<h2>Error!</h2>
-    <p>Please enter a valid number. Letters, punctuation (including commas in larger numbers), and decimals are not allowed.</p>
-    <p><a href="/">Return back</a></p>
-
-    {FOOTER}
-    """
+ </div>
+    </body>
+</html>"""
 
 @app.route("/")
 
 def index():
-    return form.format(HEADER=header, TEXT="", FOOTER=footer)
+    return form.format(TEXT='', ERROR='')
 
 @app.route("/", methods=['POST'])
 
@@ -105,7 +94,9 @@ def encrypt():
         text = request.form["text"]
         new_text = rotate_string(text, int(rot))
     except ValueError:
-        return error.format(HEADER=header, FOOTER=footer)
+        error = '''<p class="error">Please enter a valid number. Letters, punctuation 
+        (including commas in larger numbers), and decimals are not allowed.</p>'''
+        return form.format(TEXT='', ERROR=error)
     else:
-        return form.format(HEADER=header, TEXT=new_text, FOOTER=footer)
+        return form.format(TEXT=new_text, ERROR='')
 app.run()
